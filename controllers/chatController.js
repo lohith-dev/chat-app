a606322493a889a14481b012590775a08a5525a4
lohith-dev <srlohith92@gmail.com>
2023-11-21 15:02:55 +0530

    Optimising the Chat App Completed

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,22 +1,36 @@
 const msgModel=require('../models/Chat.js');
-
+const Sequelize = require('sequelize');
 
 
 const getMessages = async (req,res,next)=>{
     try {
+        const lastTimestamp = req.query.timestamp || 0;
+        console.log(lastTimestamp);
         const user = req.user;
-        const chatHistories = await user.getChatmsgs();
-        return res.status(200).json({ chat: chatHistories, message: "User chat History Fetched" })
+        const chatHistories = await user.getChatmsgs({
+            where: {
+                date_time: {
+                  [Sequelize.Op.gt]: new Date(lastTimestamp) // Retrieve messages created after the provided timestamp
+                },
+                
+              },
+              order: [['date_time', 'DESC']], 
+            limit: 10
+        });
+        const reversedResult=chatHistories.reverse();
+        return res.status(200).json({ chat: reversedResult, message: "User chat History Fetched" })
 
     } catch (error) {
         console.log(error);
         return res.status(500).json({ message: 'Internal Server error!' })
     }
 }
-const createMessage =async (req, res, next) => {
 
+
+const createMessage =async (req, res, next) => {
+   
     try {
-        console.log("llllllllllllll");
+      
         const {message}=req.body;
         console.log(req.user.id);
         const data= await msgModel.create({
